996567761a285e90c94505ae73eea4ceeba180d4
maxime <maximebiron@yahoo.fr>
2014-11-26 10:32:25 +0100

    First Draft of Client ;)

---
+++ TweetProjectClient/src/isep/tweetproject/client/ClientServlet.java
@@ -0,0 +1,38 @@
+package isep.tweetproject.client;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ClientServlet
+ */
+public class ClientServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final String JSP_LOCATION = "/WEB-INF/JSP/";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ClientServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
