d56fcf01855d26e9ea9572fdc7f2a18a04f699a5
maxime <maximebiron@yahoo.fr>
2014-11-29 18:08:26 +0100

    Ajout JSP

--- TweetProjectClient/WebContent/WEB-INF/JSP/MainJSP.jsp
+++ TweetProjectClient/WebContent/WEB-INF/JSP/MainJSP.jsp
@@ -1,12 +1,21 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
-    pageEncoding="UTF-8"%>
+	pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-<title>Insert title here</title>
+<title>Tweet Project Menu</title>
 </head>
 <body>
-
+	<div>
+		<form name="listUsers" enctype="form-data" method="post"
+			action="/TweetProjectClient/ClientServlet">
+			<input type="submit" name="Send" value="List Users"></input><br>
+			<input type="text" name="username"></input> 
+			<input type="submit" name="Send" value="Get Users tweets"></input><br>
+			<input type="submit" name="Send" value="List All Tweets"></input><br>
+			<input type="submit" name="Send" value="Fill DB"></input><br>
+		</form>
+	</div>
 </body>
 </html>
--- TweetProjectClient/src/isep/tweetproject/client/ClientServlet.java
+++ TweetProjectClient/src/isep/tweetproject/client/ClientServlet.java
@@ -1,10 +1,15 @@
 package isep.tweetproject.client;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
 
 /**
  * Servlet implementation class ClientServlet
@@ -12,27 +17,59 @@ import javax.servlet.http.HttpServletResponse;
 public class ClientServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private static final String JSP_LOCATION = "/WEB-INF/JSP/";
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ClientServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private static final String SERVER_URL = "http://localhost:8080/TweetProjectServer/rest/res/";
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	public ClientServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String jsp = JSP_LOCATION + "MainJSP.jsp";
+		RequestDispatcher dispatcher = getServletContext()
+				.getRequestDispatcher(jsp);
+		dispatcher.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		String action = request.getParameter("Send");
+		if (action == null)
+			throw new ServletException();
+
+		Client client = ClientBuilder.newClient();
+		String url = null;
+		if (action.equals("List Users")) {
+			url = SERVER_URL + "users";
+		} else if (action.equals("Get Users tweets")) {
+			String username = request.getParameter("username");
+			if (username == null) {
+				// TODO
+			} else {
+				url = SERVER_URL + "tweets?username=" + username;
+			}
+		} else if (action.equals("List All Tweets")) {
+			url = SERVER_URL + "tweets";
+		} else if (action.equals("Fill DB")) {
+			url = SERVER_URL + "update";
+		} else {
+			// Unknow : throw error
+		}
+		client.target(url).request(MediaType.APPLICATION_JSON);
 	}
 
 }
